4208c604a935f9ec11090803c1c1136e6f2ebd81
Deepak Sindhwani <dsindhwani55@gmail.com>
2020-09-03 09:57:01 +0530

    Updates

---
+++ precache-manifest.f7a392dd00ed56f0269dd3953041ed2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30bced73df18f759364c246c4e3024e8",
+    "url": "/react-monster-rolodex-live/index.html"
+  },
+  {
+    "revision": "2f5c3724dcef6c04ffc1",
+    "url": "/react-monster-rolodex-live/static/css/main.71905c70.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monster-rolodex-live/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex-live/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5c3724dcef6c04ffc1",
+    "url": "/react-monster-rolodex-live/static/js/main.ef22651c.chunk.js"
+  },
+  {
+    "revision": "33e8c5a88073a303da0f",
+    "url": "/react-monster-rolodex-live/static/js/runtime-main.15fc9945.js"
+  }
+]);
