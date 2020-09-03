574c8e901befe89215bbca3e1326795c61180f23
Deepak Sindhwani <dsindhwani55@gmail.com>
2020-09-03 09:58:29 +0530

    Added Monster Roledex to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component.jsx";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchValue: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchValue: e.target.value });
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((result) => this.setState({ monsters: result }));
+  }
+  render() {
+    const { monsters, searchValue } = this.state;
+    const filteredMonsters = monsters.filter((f) =>
+      f.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monster"
+          value={this.state.searchValue}
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
